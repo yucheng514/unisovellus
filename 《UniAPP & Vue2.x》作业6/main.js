a8bca7218540758893350aee5ed03b341e444bd1
yucheng514 <561890199@qq.com>
2024-04-14 22:56:58 +0800

    new: vue2.x hw6

---
+++ 《UniAPP & Vue2.x》作业6/main.js
@@ -0,0 +1,25 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+import store from './store'
+const app = new Vue({
+	store,
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
