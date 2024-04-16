4633716c84df02eb18669f8f8d01d4317e299b80
yucheng514 <561890199@qq.com>
2024-04-16 22:31:18 +0800

    new: hw_vue2.x_1_2

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,21 @@
 <template>
 	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<ul>
+			<li>
+				请输入第一个数：
+				<input type="text" placeholder="请输入数字" v-model="num1">
+			</li>
+			<li>
+				请输入第二个数：
+				<input type="text" placeholder="请输入数字" v-model="num2">
+			</li>
+			<li>
+				<button type="primary" @click='compare()'>比较</button>
+			</li>
+		</ul>
+		<div class="result">
+			得出结果：{{result}}
+		</div>
 	</view>
 </template>
 
@@ -10,14 +23,23 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				num1: '',
+				num2: '',
+				result: ''
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			compare() {
+				if (!this.num1 || !this.num2) {
+					this.result = '请输入正确的数字'
+				} else {
+					this.result = this.num1 == this.num2 ? '两个数相等' : parseInt(this.num1) > parseInt(this.num2) ?
+						'第一个数大于第二个数' : '第一个数小于第二个数'
+				}
+			}
 		}
 	}
 </script>
@@ -30,15 +52,22 @@
 		justify-content: center;
 	}
 
-	
-
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.compare {
+		margin: 0 auto;
+		width: 500px;
 	}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	input {
+		padding: 2rpx 2rpx;
+		border: 1rpx solid black;
+	}
+
+	ul {
+		padding: 0;
+	}
+
+	ul li {
+		list-style: none;
+		margin-top: 20px;
 	}
 </style>
---
+++ pages/sec/sec.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="content">
+		<view class="btn">
+			<button type="default" size="mini" @click="add">添加学生</button>
+			<button type="warn" @click="del" size="mini" style="margin-left: 20rpx;">删除学生</button>
+		</view>
+		
+		<uni-table style="margin-top: 20rpx;" border stripe emptyText="暂无更多数据">
+			<uni-tr>
+				<uni-th width="20" align="center">班级</uni-th>
+				<uni-th width="20" align="center">姓名</uni-th>
+				<uni-th width="20" align="center">性别</uni-th>
+				<uni-th width="20" align="center">年龄</uni-th>
+			</uni-tr>
+			<uni-tr align="center" v-for="stu in students">
+				<uni-td>{{stu.grade}}</uni-td>
+				<uni-td>{{stu.name}}</uni-td>
+				<uni-td>{{stu.gender}}</uni-td>
+				<uni-td>{{stu.age}}</uni-td>
+			</uni-tr>
+		</uni-table>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				students: []
+			}
+		},
+		methods: {
+			add() {
+				let stu = {
+					grade: '1',
+					name: '张三',
+					gender: '男',
+					age: '25'
+				}
+				this.students.push(stu)
+			},
+			del(){
+				this.students.pop()
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.btn {
+		margin-top: 20rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
