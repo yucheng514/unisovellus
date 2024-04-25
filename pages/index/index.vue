995a9bbac3a3997c83f42799c08ccda2d369f84b
yucheng514 <561890199@qq.com>
2024-04-25 10:57:58 +0800

    new: hw_vue2.x_9

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,44 +1,88 @@
 <template>
-	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+
+	<section>
+
+		<header>
+			header
+		</header>
+		<main>
+			<article class="left">
+				left
+			</article>
+			<article class="center">
+				main
+			</article>
+			<article class="right">
+				right
+			</article>
+		</main>
+		<footer>
+			footer
+		</footer>
+
+
+	</section>
+
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
-</script>
-
 <style>
-	.content {
+	html * {
+		margin: 0;
+		padding: 0;
+	}
+
+	section {
+		min-height: 100vh;
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		text-align: center;
+		line-height: 100rpx;
 	}
 
-	
-
-	.text-area {
+	main {
+		flex: 1 0 auto;
+		background-color: #6dd9bf;
 		display: flex;
-		justify-content: center;
+		/*轴向默认为row, 这里可以不写*/
+		flex-direction: row;
 	}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	header,
+	footer {
+		flex: 0 0 50rpx;
+	}
+
+
+	.left,
+	.right {
+		flex: 0 0 200rpx;
+	}
+
+	.center {
+		flex: 1 0 auto;
+	}
+
+	header {
+		background-color: #f2d680;
+	}
+
+
+	footer {
+		background-color: #50a18e;
+	}
+
+
+	.left {
+		background-color: #f2916D;
+	}
+
+	.right {
+		background-color: #f26e50;
+	}
+
+	header,
+	main,
+	footer {
+		color: #ffffff;
 	}
 </style>
